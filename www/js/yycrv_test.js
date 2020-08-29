c85aa198561e6c3344d6caa4aacc7e50d1f6541c
bytemaster <tyeenoprom@gmail.com>
2020-08-29 17:59:16 +0800

    Update

--- www/js/yycrv_test.js
+++ www/js/yycrv_test.js
@@ -24,6 +24,7 @@ async function main() {
   const yCRV_TOKEN = new ethers.Contract(yCrvTokenAddr, YCRV_ABI, App.provider);
   const yyCRV_TOKEN = new ethers.Contract(yyCrvTokenAddr, YYCRV_ABI, App.provider);
   const yCRVBalance = await yCRV_TOKEN.balanceOf(App.YOUR_ADDRESS) / 1e18;
+  const yyCRVBalance = await yyCRV_TOKEN.balanceOf(App.YOUR_ADDRESS) / 1e18;
   const yyCrvTotalSupply = await yyCRV_TOKEN.totalSupply() / 1e18;
   const yyCrvPool = await yyCRV_TOKEN.pool() / 1e18;
 
@@ -60,7 +61,7 @@ async function main() {
   _print(`\n`);
 
   _print_button_input(`Stake ${stakingTokenTicker}`, yCRVBalance, invest_yCRV);
-  _print_button_input(`Unstake`, 0, redeem);
+  _print_button_input(`Unstake ${rewardTokenTicker}`, yyCRVBalance, redeem);
 
   
   _print(`\n\n <a href="https://rinkeby.etherscan.io/address/0xe0f88584bf7e843af50c0bf3d53591566128773f#code">fake yCrv and y3d Faucet ⬅️</a>`);
